35cc9d47c3561506ecd0a7a8b50598d05cc0a55c
Andrey YURKEVICH <a.jurkevich@gmail.com>
2025-09-08 18:34:16 +0300

    Fix Follow model, self-follow issue

--- foodgram_backend/users/models.py
+++ foodgram_backend/users/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
-from django.db.models import F, Q
 from rest_framework.serializers import ValidationError
 
 from constants import (MAX_EMAIL_LENGTH, MAX_LENGTH_FIRST_NAME,
