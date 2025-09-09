c958e417d1ba06fde9a145bd91851244a12c3344
Andrey YURKEVICH <a.jurkevich@gmail.com>
2025-09-09 10:50:02 +0300

    Removed Session auth form settings.py

--- foodgram_backend/foodgram_backend/settings.py
+++ foodgram_backend/foodgram_backend/settings.py
@@ -160,7 +160,7 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
